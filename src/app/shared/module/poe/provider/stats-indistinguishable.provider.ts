3617e1d02981c0d74849bb8a3d8958b93c16992b
WhiteFang <jorisschroeven@hotmail.com>
2020-06-13 15:33:54 +0200

    Changed the stats-indistinguishable.json import based on callaborator feedback

--- src/app/shared/module/poe/provider/stats-indistinguishable.provider.ts
+++ src/app/shared/module/poe/provider/stats-indistinguishable.provider.ts
@@ -1,15 +1,5 @@
 import { Injectable } from '@angular/core'
-import {
-  crafted,
-  delve,
-  enchant,
-  explicit,
-  fractured,
-  implicit,
-  monster,
-  pseudo,
-  veiled,
-} from '../../../../../assets/poe/stats-indistinguishable.json'
+import { indistinguishableStats } from '../../../../../assets/poe/stats-indistinguishable.json'
 import { StatIndistinguishableMap, StatType } from '../type'
 
 @Injectable({
@@ -17,25 +7,6 @@ import { StatIndistinguishableMap, StatType } from '../type'
 })
 export class StatsIndistinguishableProvider {
   public provide(group: StatType): StatIndistinguishableMap {
-    switch (group) {
-      case StatType.Pseudo:
-        return pseudo
-      case StatType.Explicit:
-        return explicit
-      case StatType.Implicit:
-        return implicit
-      case StatType.Crafted:
-        return crafted
-      case StatType.Fractured:
-        return fractured
-      case StatType.Enchant:
-        return enchant
-      case StatType.Veiled:
-        return veiled
-      case StatType.Monster:
-        return monster
-      case StatType.Delve:
-        return delve
-    }
+    return indistinguishableStats[group]
   }
 }
