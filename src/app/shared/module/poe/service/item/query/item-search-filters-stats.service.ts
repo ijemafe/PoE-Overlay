6632ab2aa1a36ea5e5a5ac34a6a900c9e327b054
Kevin Strasters <kevin.strasters@gmail.com>
2020-06-13 23:42:34 -0500

    simplify the condition

--- src/app/shared/module/poe/service/item/query/item-search-filters-stats.service.ts
+++ src/app/shared/module/poe/service/item/query/item-search-filters-stats.service.ts
@@ -76,7 +76,7 @@ export class ItemSearchFiltersStatsService implements ItemSearchFiltersService {
           max = tmp
         }
       } else if (min !== undefined) {
-        if (min > 0 || (min === 0 && !stat.negated)) {
+        if (negate === 1) {
           max = 99999
         } else {
           max = min
